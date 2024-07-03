15aaeeb188efca39ec26a0b01da82b10c03f6e0f
Mark Jerly Bundalian <mbundalian@fullscale.io>
2024-07-04 00:32:49 +0800

    Create Left sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,15 +1,31 @@
+import LeftSidebar from "@/components/LeftSidebar";
+import Image from "next/image";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <div>
-      <main>
+    <div className="relative flex flex-col">
+      <main className="relative flex bg-black-3">
         {/* LEFT SIDEBAR */}
+        <LeftSidebar />
         
         {/* MAIN CONTENT */}
-        {children}
+        <section>
+          <div>
+            <div>
+              <Image />
+              MobileNav
+            </div>
+
+            <div>
+              Toasters
+              {children}
+            </div>
+          </div>
+        </section>
         
         {/* RIGHT SIDEBAR */}
       </main>
